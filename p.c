4f15907e04fd6ab4670787c2257a89e0b5b17dfb
cs19b002iittp <70936970+cs19b002iittp@users.noreply.github.com>
2023-02-28 09:21:06 +0000

    final commit

---
+++ p.c
@@ -0,0 +1,195 @@
+// C program to demonstrate peer to peer chat using Socket Programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include<errno.h>
+#include<sys/utsname.h>
+
+char name[20];
+int PORT;
+char IP[20];
+
+struct st{
+    int PORT;
+    char IP[20];
+    int nfiles;
+    int files[10];
+};
+
+#pragma pack(1)   // this helps to pack the struct to 5-bytes
+
+struct node_info
+{
+    int PORT;
+    char IP[20];
+};
+
+#pragma pack(0)   // turn packing off
+
+void sending();
+void rpc();
+void receiving(int server_fd);
+void receive_thread(void *server_fd);
+void *connection_handler(void *socket_desc,fd_set current_sockets);
+struct st nodes[100];
+int n;
+
+int main(int argc, char const *argv[])
+{
+    printf("Enter the IP address of super node : ");
+    scanf("%s",IP);
+
+    printf("Enter port number of super node:");
+    scanf("%d", &PORT);
+   
+    printf("Number of nodes: ");
+    scanf("%d",&n);
+
+    
+    for(int i=0;i<n;i++){
+        printf("Enter IP address of node %d :",i);
+        scanf("%s",nodes[i].IP);
+
+        printf("Enter port number of node %d :",i);
+        scanf("%d",&nodes[i].PORT);
+
+        printf("Enter the number of files owned by the node %d :",i);
+        int nf=0;
+        scanf("%d", &nf);
+        nodes[i].nfiles = nf;
+        for(int k=0;k<nf;k++){
+            printf("Enter the file Id: ");
+            scanf("%d",&nodes[i].files[k]);
+        }
+    }
+    
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int k = 0;
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    // Forcefully attaching socket to the port
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(IP);
+    address.sin_port = htons(PORT);
+
+    //Printed the server socket addr and port
+    printf("IP address is: %s\n", inet_ntoa(address.sin_addr));
+    printf("port is: %d\n", (int)ntohs(address.sin_port));
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 5) < 0)
+    {
+        printf("Listen Error");
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    
+    receive_thread(&server_fd);
+    close(server_fd);
+
+    return 0;
+}
+
+//Calling receiving every 2 seconds
+void receive_thread(void *server_fd)
+{
+    printf("Receive thread...\n");
+    int s_fd = *((int *)server_fd);
+    while (1)
+    {
+        sleep(2);
+        receiving(s_fd);
+    }
+}
+
+//Receiving messages on our port
+void receiving(int server_fd)
+{
+    struct sockaddr_in address;
+    int value;
+    char buffer[2000] = {0};
+    int addrlen = sizeof(address);
+    fd_set current_sockets, ready_sockets;
+    //Initialize my current set
+    FD_ZERO(&current_sockets);
+    FD_SET(server_fd, &current_sockets);
+    int k = 0;
+    while (1)
+    {
+        k++;
+        ready_sockets = current_sockets;
+
+        if (select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0)
+        {
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < FD_SETSIZE; i++)
+        {
+            if (FD_ISSET(i, &ready_sockets))
+            {
+
+                if (i == server_fd)
+                {
+                    int client_socket;
+                    if ((client_socket = accept(server_fd, (struct sockaddr *)&address,
+                                                (socklen_t *)&addrlen)) < 0)
+                    {
+                        perror("accept");
+                        exit(EXIT_FAILURE);
+                    }
+                    else{
+                        pthread_t thread_id;
+                        pthread_create( &thread_id , NULL , connection_handler , (void*) &client_socket);
+                    }
+                    FD_SET(client_socket, &current_sockets);
+                }
+            }
+        }
+
+        if (k == (FD_SETSIZE * 2))
+            break;
+    }
+}
+
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc,fd_set current_sockets)
+{
+    int i = *(int*)socket_desc;
+    int valread;
+    char buffer[2000] = {0};
+    valread = recv(i, buffer, sizeof(buffer), 0);
+    int fileId = atoi(buffer);
+                    
+    for(int z=0; z<n; z++){
+        for(int y=0; y<nodes[z].nfiles; y++){
+            if(fileId == nodes[z].files[y]){
+                struct node_info inf;
+                strcpy(inf.IP,nodes[z].IP);
+                inf.PORT=nodes[z].PORT;
+                send(i, &inf, sizeof(inf), 0);
+            }
+        }
+    }
+    FD_CLR(i, &current_sockets);
+} 
--- peer.c
+++ peer.c
@@ -13,34 +13,35 @@
 char name[20];
 int PORT;
 char IP[20];
+char supernodeIp[] = "172.17.0.1";
+int supernodePort = 5000;
 
 #pragma pack(1)   // this helps to pack the struct to 5-bytes
 
-struct system_info
+struct node_info
 {
-  char sysname[50];
-  char nodename[50];
-  char version[50];
-  char release[50];
-  char machine[50];
+    int PORT;
+    char IP[20];
 };
 
 #pragma pack(0)   // turn packing off
 
 void sending();
 void rpc();
-struct system_info info();
 void receiving(int server_fd);
 void *receive_thread(void *server_fd);
 
 int main(int argc, char const *argv[])
 {
+    int PORT_server;
+    char IP_server[20];
+
     printf("Enter name:");
     scanf("%s", name);
 
     printf("Enter your port number:");
     scanf("%d", &PORT);
-
+    
     printf("Enter your IP Address:");
     scanf("%s", IP);
 
@@ -76,21 +77,17 @@ int main(int argc, char const *argv[])
     }
     int ch;
     pthread_t tid;
-    pthread_create(&tid, NULL, &receive_thread, &server_fd); //Creating thread to keep receiving message in real time
-    printf("\n*****Select the following:*****\n1.Send message\n2.Get system info\n0.Quit\n");
+    pthread_create(&tid, NULL, &receive_thread, &server_fd); //Creating thread
+    printf("\n*****Select the following:*****\n1.Get File\n0.Quit\n");
     printf("\nEnter choice: ");
     do
     {
-
         scanf("%d", &ch);
         switch (ch)
         {
         case 1:
             sending();
             break;
-        case 2:
-            rpc();
-            break;    
         case 0:
             printf("\nLeaving\n");
             break;
@@ -104,39 +101,14 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-struct system_info info(){
-    struct utsname buf1;
-    errno =0;
-    if(uname(&buf1)!=0)
-    {
-      perror("uname doesn't return 0, so there is an error");
-      exit(EXIT_FAILURE);
-    }
-    struct system_info s1;
-    strcpy(s1.sysname,buf1.sysname);
-    strcpy(s1.nodename,buf1.nodename);
-    strcpy(s1.version,buf1.version);
-    strcpy(s1.release,buf1.release);
-    strcpy(s1.machine,buf1.machine);
-    return s1;
-}
 
-void rpc(){
+void rpc(int PORT_server,char IP_server[20],struct node_info*inf,int fileId){ //Fetching port number
+    printf("RPC called %d\n", fileId);
     char buffer[2000] = {0};
-    //Fetching port number
-    int PORT_server;
-    char IP_server[20];
-
-    //IN PEER WE TRUST
-    printf("Enter the port to send message:"); //Considering each peer will enter different port
-    scanf("%d", &PORT_server);
-
-    printf("Enter the IP address:");
-    scanf("%s", IP_server);
-
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
     char hello[1024] = {0};
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
@@ -146,6 +118,7 @@ void rpc(){
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(IP_server); // gives an IP
     serv_addr.sin_port = htons(PORT_server);
+    printf("rpc %d %s \n",PORT_server,IP_server);
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
@@ -153,36 +126,31 @@ void rpc(){
         return;
     }
 
-    sprintf(buffer, "Getting system info");
-    send(sock, buffer, sizeof(buffer), 0);
-
-    struct system_info result;
-    recv(sock, &result, sizeof(struct system_info), 0);
-    printf("\nSystem Info\n");
-    printf("System Name = %s\n",result.sysname);
-    printf("Node Name = %s\n",result.nodename);
-    printf("Version = %s\n",result.version);
-    printf("Release = %s\n",result.release);
-    printf("Machine = %s\n",result.machine);
+    else{
+        printf("Sending rpc..\n");
+        sprintf(buffer, "%d",fileId);
+        send(sock, buffer, sizeof(buffer), 0);
+        recv(sock, inf, sizeof(struct node_info), 0);
+        printf("Received rpc\n");
+    }
+    
     close(sock);
-    // sprintf("Getting system info");
 }
 
 //Sending messages to port
 void sending()
 {
+    int fileId;
+    printf("Enter the file Id to be fetched : ");
+    scanf("%d",&fileId);
+
+    struct node_info result;
+    rpc(supernodePort,supernodeIp,&result,fileId);
+
     char buffer[2000] = {0};
     //Fetching port number
     int PORT_server;
     char IP_server[20];
-
-    //IN PEER WE TRUST
-    printf("Enter the port to send message:"); //Considering each peer will enter different port
-    scanf("%d", &PORT_server);
-
-    printf("Enter the IP address:");
-    scanf("%s", IP_server);
-
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
     char hello[1024] = {0};
@@ -193,8 +161,10 @@ void sending()
     }
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(IP_server); // gives an IP
-    serv_addr.sin_port = htons(PORT_server);
+    serv_addr.sin_addr.s_addr = inet_addr(result.IP); // gives an IP
+    serv_addr.sin_port = htons(result.PORT);
+
+    printf("Calling node with IP %s, PORT %d, for file %d\n", result.IP, result.PORT, fileId);
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
@@ -202,13 +172,28 @@ void sending()
         return;
     }
 
+    sprintf(buffer, "%d",fileId);
+
     char dummy;
-    printf("Enter your message:");
-    scanf("%c", &dummy); //The buffer is our enemy
-    scanf("%[^\n]s", hello);
-    sprintf(buffer, "%s[IP:%s][PORT:%d] says: %s", name, IP, PORT, hello);
+    
     send(sock, buffer, sizeof(buffer), 0);
-    printf("\nMessage sent\n");
+    recv(sock,buffer,sizeof(buffer),0);
+
+    // printf("Content of the file %d.txt is:\n\n%s\n",fileId, buffer);
+    FILE *fptr;
+    char path[50];
+    sprintf(path, "%s/%d.txt",name, fileId);
+    fptr = fopen(path,"w");
+
+    if(fptr == NULL)
+    {
+        printf("Error!");   
+        exit(1);             
+    }
+
+    fprintf(fptr,"%d",sizeof(buffer));
+    printf("File %d.txt is saved to folder %s\n",fileId, name);
+    fclose(fptr);
     close(sock);
 }
 
@@ -228,8 +213,7 @@ void receiving(int server_fd)
 {
     struct sockaddr_in address;
     int valread;
-    int value;
-    struct system_info s2;
+    int value=0;
     char buffer[2000] = {0};
     int addrlen = sizeof(address);
     fd_set current_sockets, ready_sockets;
@@ -269,14 +253,29 @@ void receiving(int server_fd)
                 else
                 {
                     valread = recv(i, buffer, sizeof(buffer), 0);
-                    value=strcmp(buffer,"Getting system info");  
-                    if(value==0){
-                        s2 = info();
-                        send(i, &s2, sizeof(s2), 0);
-                    }
-                    else{
-                        printf("\n%s\n", buffer);
-                    }
+                    printf("\nRequest for file buffer%s.txt\n", buffer);
+                    char result[2000];
+                    FILE    *textfile;
+                    char    *text;
+                    long    numbytes;
+                    char path[100];
+                    sprintf(path,"%s/%s.txt",name,buffer);
+                
+                    textfile = fopen(path, "r");
+                    if(textfile == NULL)
+                        return;
+                    
+                    fseek(textfile, 0L, SEEK_END);
+                    numbytes = ftell(textfile);
+                    fseek(textfile, 0L, SEEK_SET);  
+                
+                    text = (char*)calloc(numbytes, sizeof(char));   
+                    if(text == NULL)
+                        return;
+                
+                    fread(text, sizeof(char), numbytes, textfile);
+                    fclose(textfile);
+                    send(i, text, numbytes, 0);
                     FD_CLR(i, &current_sockets);
                 }
             }
